79c4626fa980adab7ea3223ce348f07a1c5fa246
James Cox <j-cox03@hotmail.com>
2017-10-25 21:11:52 +0100

    video 11

--- blog/views.py
+++ blog/views.py
@@ -33,21 +33,23 @@ def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status='published')
     sent = False
 
+    form = None
     if(request.method == 'POST'):
         form = EmailPostForm(request.POST)
-        if(form.is_valid?()):
+        if(form.is_valid()):
             cd = form.cleaned_data
-            post_url = request.build_absolute_url(post.get_absolute_url())
-            subject = '{} ({}) recommends you read "{}"'.format(cd['name'], cd['email'], post['title'])
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{} ({}) recommends you read "{}"'.format(cd['name'], cd['email'], post.title)
             message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post_url, '1', '2')
-            send_mail(subject, message, 'admin@blog.com' [cd['to']])
+            # send_mail(subject, message, 'admin@blog.com', [cd['to']])
+            print('send does not work due to SMTPDataError')
             sent = True
-        else:
-            form = EmailPostForm()
+    else:
+        form = EmailPostForm()
 
     to_send = {
-        'post': post
-        'form': form
+        'post': post,
+        'form': form,
         'sent': sent
     }
     return render(request, 'blog/post/share.html', to_send)
